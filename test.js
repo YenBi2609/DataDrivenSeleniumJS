eefe3f0058b4cf8d20eee8179e9af901ee549c12
unknown <yenbi2609@gmail.com>
2021-09-20 22:57:27 +0700

    commit

--- test.js
+++ test.js
@@ -1,21 +1,30 @@
 const { Builder, By, Key } = require('selenium-webdriver');
 var Excel = require('exceljs');
+const video = require('wdio-video-reporter');
 
-let driver = new Builder().forBrowser("chrome").build();
+let driver = new Builder().forBrowser("firefox").build();
 
 var inboundWorkbook = new Excel.Workbook();
-inboundWorkbook.xlsx.readFile("C:/data.xlsx").then(async function () {
+inboundWorkbook.xlsx.readFile("./data.xlsx").then(async function () {
     var inboundWorksheet = inboundWorkbook.getWorksheet(1); //or name of the worksheet
+    //  a = inboundWorksheet.getRow(2).getCell('B').value;
+    //  console.log(a);
+    //  var row = inboundWorksheet.actualRowCount;
+    //  console.log(row);
+    //  var column = inboundWorksheet.actualRowCount;
+    //  console.log(column);
+    //  b = inboundWorksheet.getColumn(1).values;
+    //  console.log(b);
 
-      driver.get(inboundWorksheet.getCell('A2').value);
+     driver.get(inboundWorksheet.getCell('A2').value);
 
  
-        await driver.findElement(By.name("q")).sendKeys(inboundWorksheet.getCell('B2').value,Key.RETURN);
+       await driver.findElement(By.name("q")).sendKeys(inboundWorksheet.getCell('B2').value,Key.RETURN);
  
-        var title = await driver.getTitle();
-        console.log('Title is:',title);
+       var title = await driver.getTitle();
+       console.log('Title is:',title);
  
-        await driver.quit();
+       await driver.quit();
 });
 
 

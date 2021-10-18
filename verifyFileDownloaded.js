a82c872118604a3a6a19a8bad59d0129d18c4bf7
unknown <yenbi2609@gmail.com>
2021-10-18 10:30:51 +0700

    read file in directory 2nd

--- verifyFileDownloaded.js
+++ verifyFileDownloaded.js
@@ -13,19 +13,33 @@ var Excel = require('exceljs');
 //     }
 //   }
 // );
-var homedir = "C://Users/Admin/Downloads";
+var dir = "C://Users/Admin/Downloads";
+const { join } = require('path');
 
-var files = fs.readdirSync(homedir);
-var inboundWorkbook = new Excel.Workbook();
-for (var i in files) {
-  if (path.extname(files[i]) === ".xlsx") {
-    //do something
-    console.log(files[i]);
-    console.log(i);
-     inboundWorkbook.xlsx.readFile('C://Users/Admin/Downloads/' + files[i] + '').then(async function () {
-      var inboundWorksheet = inboundWorkbook.getWorksheet(1); //or name of the worksheet
-      var row = inboundWorksheet.actualRowCount;
-      console.log(row);
-    });
+var files = fs.readdirSync(dir)
+  .filter((file) => fs.lstatSync(join(dir, file)).isFile())
+  .map((file) => ({ file, mtime: fs.lstatSync(join(dir, file)).mtime }))
+  .sort((a, b) => b.mtime.getTime() - a.mtime.getTime());
+  for( var i in files){
+    var a = (/[.]/.exec(files[i].file)) ? /[^.]+$/.exec(files[i].file) : undefined
+    if(a[0]=='xlsx'){
+    console.log(a.input);
+    }
   }
-}
+// var inboundWorkbook = new Excel.Workbook();
+// for (var i in files) {
+//   if (path.extname(files[i]) === ".xlsx") {
+//     console.log(files[i]);
+//      console.log(i);
+//     //  stats = fs.statSync(resolve(join('C://Users/Admin/Downloads', files[i])));
+//     //  sort((a, b) => b.mtime.getTime() - a.mtime.getTime());
+//     //  console.log(stats.mtime.getTime());
+//     //  console.log(stats.ctime);
+//     //  inboundWorkbook.xlsx.readFile('C://Users/Admin/Downloads/' + files[i] + '').then(async function () {
+//     //   var inboundWorksheet = inboundWorkbook.getWorksheet(1); //or name of the worksheet
+//     //   var row = inboundWorksheet.actualRowCount;
+//     //   console.log(row);
+//     // });
+//   }
+// }
+
